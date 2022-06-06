a686631e2a4839f898c85a4523fa6b1e536279aa
Rafet <professionalraf@gmail.com>
2022-06-07 02:15:39 +0400

    Static Lesson

---
+++ Static Lesson/Static Lesson/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Static_Lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string groupNo;
+            do
+            {
+                Console.WriteLine("Group No daxil edin");
+                 groupNo = Console.ReadLine();
+            } while (!Student.CheckGroupNo(groupNo));
+
+            Student student = new Student();
+            student.GroupNo = groupNo;
+            Console.WriteLine(student.GroupNo);
+           
+            
+        }
+    }
+}
---
+++ Static Lesson/Static Lesson/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Static_Lesson
+{
+    internal class Student
+    {
+        public Student()
+        {
+            _mainNo++;
+            No = _mainNo;
+        }
+        public readonly int No;
+        public string Fullname;
+        private string _groupNo;
+        private static int _mainNo;
+
+        public string GroupNo
+        {
+            get => _groupNo;
+            set
+            {
+                if (CheckGroupNo(value))
+                {
+                    _groupNo = value;
+                }
+            }
+
+            
+        }
+        public static bool CheckGroupNo(string value)
+        {
+            if (Char.IsLetter(value[0]) && value.Length == 4)
+            {
+                for (int i = 1; i < value.Length; i++)
+                {
+                    if (!Char.IsDigit(value[i]))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+}
